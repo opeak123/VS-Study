0d62e7642c627247bbead579d2e9d6d2f0ddd64b
opeak123 <opeak123@hanmail.net>
2023-09-12 17:40:41 +0900

    23-09-12  노드생성 이해

--- c/2023-09-11-c/SingleList_ex/ex0.c
+++ c/2023-09-11-c/SingleList_ex/ex0.c
@@ -29,7 +29,8 @@ int InsertNode(char* newData)
 	memset(newNode,0,sizeof(Node));
 	//복사
 	strcpy_s(newNode->nData, sizeof(newNode->nData), newData);
-	
+	//strncpy_s(newNode->nData, sizeof(newNode->nData), newData, _TRUNCATE);
+	// strncpy_s는 버퍼크기를 맞게 자르거나 제어할때
 
 	if (g_head == NULL)
 	{
@@ -37,19 +38,33 @@ int InsertNode(char* newData)
 	}
 	else
 	{
-		newNode->next = g_head; //원본코드(맞음)
-		//g_head->next = newNode->nData; 틀림 (내가작성한거)
-		g_head = newNode; //원본코드(맞음)
-	}
-	return 1;
+		newNode->next = g_head;
+		g_head = newNode; //순서 바꾸면 데이터 유실
+	} 
+	return 1; 
 }
 
+void PrintNode(void)
+{ 
+	Node* p_head = g_head;
+	while (p_head != NULL)
+	{
+		printf("[주소값: %p에 들어있는 데이터 : %s] = ", p_head, p_head->nData);
+		printf("[다음 next값 : %p]\n", p_head->next);
+		p_head = p_head->next;
+
+	}
+	putchar('\n');
+}
 
 
 int main()
 {
 
 	InsertNode("sad");
-	//PrintList();
+	//PrintNode();
+
+	InsertNode("happy");
+	PrintNode();
 	return 0;
 }
