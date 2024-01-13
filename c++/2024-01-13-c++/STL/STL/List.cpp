433cebdc408de751cce1b9359769ec75746c4670
opeak123 <opeak123@hanmail.net>
2024-01-13 19:50:12 +0900

    24-01-13 List

---
+++ c++/2024-01-13-c++/STL/STL/List.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <list>
+//
+//
+//
+//void PrintList(std::list<int> list)
+//{
+//
+//	for (auto it = list.begin(); it != list.end(); it++)
+//	{
+//		std::cout << *it << " ";
+//	}
+//	std::cout << std::endl;
+//}
+//
+//int main(void)
+//{
+//
+//	std::list<int> list = { 1,2,3,4,5};
+//	PrintList(list);
+//
+//	auto it = std::find(list.begin(), list.end(), 3);
+//
+//	list.insert(it,10);
+//	PrintList(list);
+//
+//	//list.erase(it);
+//
+//	std::cout << *(it) << std::endl;
+//
+//
+//	return 0;
+//}
--- c++/2024-01-13-c++/STL/STL/Vector.cpp
+++ c++/2024-01-13-c++/STL/STL/Vector.cpp
@@ -1,52 +1,52 @@
-#include <iostream>
-#include <vector>
-
-
-class Person
-{
-private:
-	std::string name;
-
-
-public:
-	Person(std::string name)
-		:name{ name } {
-		std::cout << "called" << std::endl;
-	}
-};
-
-int main(void)
-{
-	/*std::vector<int> vec = { 1,2,3,4,5};
-
-	std::vector<int>::iterator it;
-
-	vec.push_back(9099);
-	
-	std::cout << vec.size() << std::endl;
-	std::cout << vec.capacity() << std::endl;
-	std::cout << vec[0] << std::endl;
-	std::cout << vec.at(5) << std::endl;
-
-	
-
-	std::vector<Person> p1;
-
-	p1.push_back(Person{"lee"});
-	p1.emplace_back(Person{ "kim" });
-
-	p1.pop_back();
-	p1.pop_back();
-
-	std::cout << p1.size() << std::endl;*/
-
-
-	std::vector<int> vect = { 1,2,3,4,5 };
-
-	auto it = std::find(vect.begin(), vect.end(), 1);
-	
-	vect.insert(it, 7);
-	std::cout << vect[0] << std::endl;
-	return 0;
-
-}
+//#include <iostream>
+//#include <vector>
+//
+//
+//class Person
+//{
+//private:
+//	std::string name;
+//
+//
+//public:
+//	Person(std::string name)
+//		:name{ name } {
+//		std::cout << "called" << std::endl;
+//	}
+//};
+//
+//int main(void)
+//{
+//	/*std::vector<int> vec = { 1,2,3,4,5};
+//
+//	std::vector<int>::iterator it;
+//
+//	vec.push_back(9099);
+//	
+//	std::cout << vec.size() << std::endl;
+//	std::cout << vec.capacity() << std::endl;
+//	std::cout << vec[0] << std::endl;
+//	std::cout << vec.at(5) << std::endl;
+//
+//	
+//
+//	std::vector<Person> p1;
+//
+//	p1.push_back(Person{"lee"});
+//	p1.emplace_back(Person{ "kim" });
+//
+//	p1.pop_back();
+//	p1.pop_back();
+//
+//	std::cout << p1.size() << std::endl;*/
+//
+//
+//	std::vector<int> vect = { 1,2,3,4,5 };
+//
+//	auto it = std::find(vect.begin(), vect.end(), 1);
+//	
+//	vect.insert(it, 7);
+//	std::cout << vect[0] << std::endl;
+//	return 0;
+//
+//}
