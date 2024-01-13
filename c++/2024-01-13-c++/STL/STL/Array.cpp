20897228667d85884d1be89250e4ae78927d12fd
opeak123 <opeak123@hanmail.net>
2024-01-13 19:31:52 +0900

    2024-01-13 Vector

---
+++ c++/2024-01-13-c++/STL/STL/Array.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <array>
+//#include <algorithm>
+//#include <string>
+//
+//int main(void)
+//{
+//	std::array<int, 5> ray = { 1,2,3,4,5};
+//	std::array<std::string, 3> ray2 = { "kim","lee","park" };
+//
+//
+//	//std::cout << ray.size() << std::endl;
+//	//std::cout << ray[0] << std::endl;
+//	//std::cout << ray.at(1) << std::endl;
+//	ray.fill(10);
+//	std::cout << ray.data() << std::endl;
+//	/*for (auto it = ray.begin(); it != ray.end(); it++)
+//	{
+//		std::cout << *it << std::endl;
+//	}*/
+//
+//
+//
+//	return 0;
+//}
