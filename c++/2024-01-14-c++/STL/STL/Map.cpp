463f04fd1eeaa7293cff72c03e0907c7e9d27c9a
opeak123 <opeak123@hanmail.net>
2024-01-14 14:20:01 +0900

    2024-01-14 Map

---
+++ c++/2024-01-14-c++/STL/STL/Map.cpp
@@ -0,0 +1,46 @@
+//#include <iostream>
+//#include <map>
+//#include <unordered_set>
+//#include <unordered_map>
+//#include <string>
+//#include <vector>
+//int main(void)
+//{
+//	std::map < std::string, int> map1 =
+//	{ {"Kim",20},{"Lee",21},{"Jun",19}, {"Kim",88} };
+//	
+//	auto it = map1.find("Kim");
+//
+//	std::cout << it->second << std::endl;
+//
+//	map1["Kim"] = 55;
+//	std::cout << map1["Kim"] << std::endl;
+//
+//
+//	map1["Park"] = 25;
+//	std::cout << map1["Park"] << std::endl;
+//	map1.insert(std::make_pair("Choi", 30));
+//	std::cout << map1["Choi"] << std::endl;
+//
+//
+//	/*std::vector<int> vec = { 1,2,3 };
+//	std::vector<int>::reverse_iterator it1 = vec.rbegin();
+//	while (it1 != vec.rend())
+//	{
+//		std::cout << *it1 << " ";
+//		it1++;
+//	}*/
+//
+//
+//
+//
+//	/*map1.insert(std::make_pair("k", 15));
+//
+//	for (auto it = map1.begin(); it != map1.end(); ++it)
+//	{
+//		std::cout << it->first << it->second << " ";
+//	}*/
+//
+//	return 0;
+//
+//}
