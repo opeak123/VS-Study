a8e4f7f232e0c773bfbfeabf260c710909c2abf1
opeak123 <opeak123@hanmail.net>
2024-01-25 23:21:14 +0900

    24-01-25 dictionary

---
+++ python/24-01-25/dictionary.py
@@ -0,0 +1,5 @@
+dic = {'name': 'Eric', 'age': 25}
+
+print(dic)
+
+#for k, v in dic.items:
---
+++ python/24-01-25/main.py
@@ -0,0 +1,11 @@
+# a = "python"
+# b = ["1","4","2"]
+# b.append("5")
+# b.sort()
+# print(b)
+
+
+# c = (4,5,1)
+
+# #c.sort()
+# print(c)
